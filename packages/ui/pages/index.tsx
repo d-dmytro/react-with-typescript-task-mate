2b6d6d6d6f00549f9a3a812bd097ccf0ba07399d
Dima <d-dmytro@users.noreply.github.com>
2019-03-21 14:20:32 +0200

    Fetch the tasks and render them

--- packages/ui/pages/index.tsx
+++ packages/ui/pages/index.tsx
@@ -1,8 +1,39 @@
 import { Layout } from '../components/Layout';
 import TASKS_QUERY from '../graphql/tasks.graphql';
+import { Query } from 'react-apollo';
+import {
+  TasksQuery as ITasksQuery,
+  TasksQueryVariables
+} from '../resources/gql-types';
+import { Loader } from '../components/Loader';
+import { Task } from '../components/Task';
+
+class TasksQuery extends Query<ITasksQuery, TasksQueryVariables> {}
 
 export default () => (
   <Layout>
-    <div>Hello World!</div>
+    <TasksQuery query={TASKS_QUERY}>
+      {({ error, loading, data }) => {
+        if (error) {
+          return <p>Something wrong happened</p>;
+        }
+
+        const tasks = data ? data.tasks : [];
+
+        return (
+          <div>
+            {loading ? (
+              <Loader />
+            ) : (
+              <ul>
+                {tasks.map((task, i) => {
+                  return <Task key={i} {...task} />;
+                })}
+              </ul>
+            )}
+          </div>
+        );
+      }}
+    </TasksQuery>
   </Layout>
 );
